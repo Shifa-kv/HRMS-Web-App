7fac842207b4fe26dcc4f7ace659dc8efd1e451b
shifa123@ <fathimashifakv@gmail.com>
2023-08-27 01:07:28 +0530

    dashboard updated!!!

--- src/Components/Admin/Dashboard/ProgressBanner.tsx
+++ src/Components/Admin/Dashboard/ProgressBanner.tsx
@@ -1,11 +1,13 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { firestore } from "../../../Firebase/Config";
 import ProgressCircle from "../../Graphs/ProgressCircle";
 import { FaClipboardList, FaDiagramProject, FaUserPlus, FaUsers } from 'react-icons/fa6';
 import CalculatePercentage from "../../../Utils/CalculatePercentage";
 
 const ProgressBanner = () => {
-    const [Users, setUsers] = useState<any>();
+  const [Users, setUsers] = useState<any>();
+  const [data, setData] = useState<any>();
+
   useEffect(() => {
     firestore.collection('users').onSnapshot((snapshot) => {
       const userData = snapshot.docs.map((user) => {
@@ -16,6 +18,14 @@ const ProgressBanner = () => {
       })
       setUsers(userData);
     })
+    firestore.collection("leaves").where('status','==','approved')
+      .get().then((snapshot) => {
+        const values = snapshot.docs.map((res) => {
+          const datas = res.data();
+          return { ...datas, id: res.id };
+        })
+        setData(values)
+      })
   }, []);
 
   const filterUser = (data: string, value: string | boolean | number) => {
@@ -31,43 +41,60 @@ const ProgressBanner = () => {
       return userDate >= threeMonthsAgo
     })
   }
+
+  const getLeaves = () => {
+    const today = new Date();
+
+    const todayLeaves: number = data?.filter((doc: any) => {
+        const dateObj = new Date(doc.start?.seconds * 1000);
+        const dateEndObj = new Date(doc.end?.seconds * 1000);
+        return dateObj.getDate() <= today.getDate() &&
+            dateObj.getFullYear() <= today.getFullYear() &&
+            dateObj.getMonth() <= today.getMonth() &&
+            dateEndObj.getDate() >= today.getDate() &&
+            dateEndObj.getFullYear() >= today.getFullYear() &&
+            dateEndObj.getMonth() >= today.getMonth()
+    })?.length;
+    return todayLeaves
+
+  }
   return (
     <div className='container bg-gradient-to-b from-defaultBg to-transparent'>
-        <div className='flex space-x-6 justify-between'>
-          <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
-            <FaUsers size={50} className='bg-color-two text-defaultBg p-3 rounded-full' />
-            <h6>Permenent Employees</h6>
-            <h3>{filterUser('jobstatus', 'fulltime')?.length}<span className=' text-base'>/{Users?.length}</span></h3>
+      <div className='flex space-x-6 justify-between'>
+        <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
+          <FaUsers size={50} className='bg-color-two text-defaultBg p-3 rounded-full' />
+          <h6>Permenent Employees</h6>
+          <h3>{filterUser('jobstatus', 'fulltime')?.length}<span className=' text-base'>/{Users?.length}</span></h3>
+        </div>
+        <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
+          <FaClipboardList size={50} className='bg-color-one text-defaultBg p-3 rounded-full' />
+          <h6>On leave</h6>
+          <h3>{getLeaves()}<span className=' text-base'>/{Users?.length}</span></h3>
+        </div>
+        <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
+          <FaUserPlus size={50} className='bg-stone-400 text-defaultBg p-3 rounded-full' />
+          <h6>New joinee</h6>
+          <h3>{getNewjoineeCount()?.length}<span className=' text-base'>/{Users?.length}</span></h3>
+        </div>
+        <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
+          <FaDiagramProject size={50} className='bg-color-three text-defaultBg p-3 rounded-full' />
+          <h6>Projects pending</h6>
+          <h3>0<span className=' text-base'>/0</span></h3>
+        </div>
+        <div className='w-3/12 border h-max border-defaultBg rounded-lg p-4 flex space-x-4 justify-around items-center bg-white'>
+          <ProgressCircle text={Users?.length} value={CalculatePercentage((filterUser('jobmode', 'remote')?.length), (Users?.length))} height='107px' />
+          <div className=' h-max text-[14px] '>
+            <p className='relative before:absolute before:left-[-12px] before:top-2 before:h-2 before:w-2 before:rounded-full before:p-[-10px] before:bg-color-two before:block flex'>
+              {filterUser('jobmode', 'remote')?.length} Remote
+            </p>
+            <p className='relative before:absolute before:left-[-12px] before:top-2 before:h-2 before:w-2 before:rounded-full before:p-[-10px] before:bg-defaultBg before:block flex'>
+              {filterUser('jobmode', 'office')?.length} Office
+            </p>
           </div>
-          <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
-            <FaClipboardList size={50} className='bg-color-one text-defaultBg p-3 rounded-full' />
-            <h6>On leave</h6>
-            <h3>1<span className=' text-base'>/{Users?.length}</span></h3>
-          </div>
-          <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
-            <FaUserPlus size={50} className='bg-stone-400 text-defaultBg p-3 rounded-full' />
-            <h6>New joinee</h6>
-            <h3>{getNewjoineeCount()?.length}<span className=' text-base'>/{Users?.length}</span></h3>
-          </div>
-          <div className='w-2/12 border border-defaultBg rounded-lg p-4 bg-white'>
-            <FaDiagramProject size={50} className='bg-color-three text-defaultBg p-3 rounded-full' />
-            <h6>Projects pending</h6>
-            <h3>0<span className=' text-base'>/0</span></h3>
-          </div>
-          <div className='w-3/12 border h-max border-defaultBg rounded-lg p-4 flex space-x-4 justify-around items-center bg-white'>
-            <ProgressCircle text={Users?.length} value={CalculatePercentage((filterUser('jobmode', 'remote')?.length), (Users?.length))} height='107px' />
-            <div className=' h-max text-[14px] '>
-              <p className='relative before:absolute before:left-[-12px] before:top-2 before:h-2 before:w-2 before:rounded-full before:p-[-10px] before:bg-color-two before:block flex'>
-                {filterUser('jobmode', 'remote')?.length} Remote
-              </p>
-              <p className='relative before:absolute before:left-[-12px] before:top-2 before:h-2 before:w-2 before:rounded-full before:p-[-10px] before:bg-defaultBg before:block flex'>
-                {filterUser('jobmode', 'office')?.length} Office
-              </p>
-            </div>
 
-          </div>
         </div>
       </div>
+    </div>
   )
 }
 export default ProgressBanner
